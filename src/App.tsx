afaa0405a5b12b6194c4e9af39d147ea231b9d82
Felipe Dev <flimalimafelipek@gmail.com>
2026-03-23 08:29:18 -0300

    feat: configuração de organização de classes do taiwild

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import "./config/global.css";
+import './config/global.css'
 
 function App() {
-  return <h1>StudioVit - Desinger and marketing</h1>;
+  return (
+    <div className="flex items-center rounded-md bg-green-500 px-4 py-2 text-white">
+      <h1 className="font-family-primary">
+        StudioVit - Desinger and marketing
+      </h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
